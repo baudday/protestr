71feef7d92b342c105fb15fa67d51387c1ba4861
Willem Ellis <willem.ellis@gmail.com>
2014-09-05 15:25:21 -0400

    multiple post-deploy scripts

---
+++ post-deploy.php
@@ -0,0 +1,6 @@
+<?php
+
+`php artisan migrate`;
+`php artisan asset:publish --path="vendor/twbs/bootstrap/dist/" bootstrap`;
+`php artisan asset:publish --path="vendor/components/jquery/" jquery`;
+
