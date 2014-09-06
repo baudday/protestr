c4494cb4ae39988bccdf7a48a3b3924dd9d206c2
Willem Ellis <willem.ellis@gmail.com>
2014-09-06 11:13:27 -0400

    uncomment routes

--- app/routes.php
+++ app/routes.php
@@ -1,46 +1,42 @@
 <?php
 
-Route::resource('/', 'SplashController', [
-  'only' => ['index', 'store']
+Route::get('/', [
+  'as' => 'home',
+  'uses' => 'PagesController@index'
 ]);
 
-Route::get('/thanks', [
-  'as' => 'thanks',
-  'uses' => 'SplashController@thanks'
+/**
+ * Users methods
+ */
+Route::get('/signup', [
+  'as' => 'signup',
+  'uses' => 'UsersController@create',
+]);
+Route::get('/profile/{username}', [
+  'as' => 'profile',
+  'uses' => 'UsersController@show'
+]);
+Route::resource('/users', 'UsersController', [
+  'only' => ['create', 'store', 'show']
 ]);
 
-// /**
-//  * Users methods
-//  */
-// Route::get('/signup', [
-//   'as' => 'signup',
-//   'uses' => 'UsersController@create',
-// ]);
-// Route::get('/profile/{username}', [
-//   'as' => 'profile',
-//   'uses' => 'UsersController@show'
-// ]);
-// Route::resource('/users', 'UsersController', [
-//   'only' => ['create', 'store', 'show']
-// ]);
 
+/**
+ * Session methods
+ */
+Route::get('/login', [
+  'as' => 'login',
+  'uses' => 'SessionsController@create',
+]);
+Route::get('/logout', [
+  'as' => 'logout',
+  'uses' => 'SessionsController@destroy'
+]);
+Route::resource('/sessions', 'SessionsController', [
+  'only' => ['create', 'store', 'destroy']
+]);
 
-// /**
-//  * Session methods
-//  */
-// Route::get('/login', [
-//   'as' => 'login',
-//   'uses' => 'SessionsController@create',
-// ]);
-// Route::get('/logout', [
-//   'as' => 'logout',
-//   'uses' => 'SessionsController@destroy'
-// ]);
-// Route::resource('/sessions', 'SessionsController', [
-//   'only' => ['create', 'store', 'destroy']
-// ]);
-
-// /**
-//  * Protests routes
-//  */
-// Route::resource('/protests', 'ProtestsController');
+/**
+ * Protests routes
+ */
+Route::resource('/protests', 'ProtestsController');
