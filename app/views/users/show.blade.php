6ea3cb5b44b52e32bbe929ac2fa4f0be424f94bd
Willem Ellis <willem.ellis@gmail.com>
2014-09-08 19:44:25 +0000

    playing around with profiles a bit

--- app/views/users/show.blade.php
+++ app/views/users/show.blade.php
@@ -2,13 +2,24 @@
 
 @section('content')
   <div class="row">
-    <div class="col-sm-8 col-sm-offset-2">
+    <div class="col-xs-12">
       <h1>{{{ $user->username }}}'s profile</h1>
       {{ gravatar_tag($user->email, array('s' => 200, 'd' => 'identicon')) }}
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-sm-6">
+      <h2>Protests I started</h2>
+      @foreach($user->protests as $protest)
+        <h3>{{ link_to_route('protests.show', $protest->mission, $protest->id) }} <small class="time">{{{ $protest->when_date }}}</small></h3>
+        <p>{{{ str_limit($protest->history, 140) }}}</p>
+      @endforeach
+    </div>
+    <div class="col-sm-6">
       <h2>Protests I'm attending</h2>
       @foreach($user->attending as $protest)
         <h3>{{ link_to_route('protests.show', $protest->mission, $protest->id) }} <small class="time">{{{ $protest->when_date }}}</small></h3>
-        <p>{{{ str_limit($protest->history) }}}</p>
+        <p>{{{ str_limit($protest->history, 140) }}}</p>
       @endforeach
     </div>
   </div>
