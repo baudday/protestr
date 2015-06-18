799bd47f9340307040b7d2442f04b10b4fd9be3e
Willem Ellis <willem.ellis@gmail.com>
2015-06-18 19:56:35 +0000

    add title and back button to update create page

--- app/views/updates/create.blade.php
+++ app/views/updates/create.blade.php
@@ -3,6 +3,8 @@
 @section('content')
   <div class="row">
     <div class="col-md-6 col-md-offset-3">
+      <a href="{{ route('protests.show', $protest->id) }}"><span class="glyphicon glyphicon-chevron-left"></span> back to protest</a>
+      <h1>post an update</h1>
       @if(Session::get('message'))
         <div class="alert alert-{{ Session::get('message')['class'] }}">
           {{ Session::get('message')['message'] }}
