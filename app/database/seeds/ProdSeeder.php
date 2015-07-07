5126a8f8743a9e396a90b8f681d5ec6463b10aa7
Willem Ellis <willem.ellis@gmail.com>
2015-07-07 17:18:33 +0000

    production db seeder

---
+++ app/database/seeds/ProdSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+class ProdSeeder extends Seeder {
+
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Eloquent::unguard();
+
+        $this->call('TopicTableSeeder');
+    }
+
+}
